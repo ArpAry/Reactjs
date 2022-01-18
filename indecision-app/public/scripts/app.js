fdc6f014ee46aef2f7945672f4fecb1a0c484f59
ArpAry <arpitarya3299@gmail.com>
2022-01-18 23:09:48 +0530

    sjdfsdhjkfhjhfkjdlhkjafhdsifkhkj

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -2,55 +2,82 @@
 
 console.log("hello");
 ///////////JSX  Java Script XML
-var studentInfo = React.createElement(
-      "div",
-      null,
-      React.createElement(
-            "h1",
-            null,
-            "Name: Arpit"
-      ),
-      React.createElement(
-            "p",
-            null,
-            " Age:20"
-      ),
-      React.createElement(
-            "p",
-            null,
-            "Location:Bareilly"
-      )
-);
-var templateTwo = document.getElementById("personal");
-ReactDOM.render(studentInfo, templateTwo);
+var app = {
+  title: "App",
+  subtitle: "this app is very useful",
+  options: ['one', 'two']
 
+};
 var template = React.createElement(
-      "div",
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  app['options'].length > 0 ? React.createElement(
+    "p",
+    null,
+    "Here are Your Options"
+  ) : React.createElement(
+    "p",
+    null,
+    "No Options "
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
       null,
-      React.createElement(
-            "h1",
-            null,
-            "INDECISION APP"
-      ),
-      React.createElement(
-            "p",
-            null,
-            "this is your jsx code"
-      ),
-      React.createElement(
-            "ol",
-            null,
-            React.createElement(
-                  "li",
-                  null,
-                  "HTML"
-            ),
-            React.createElement(
-                  "li",
-                  null,
-                  "CSS"
-            )
-      )
+      "Item One"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item Two"
+    )
+  )
 );
 var appRoot = document.getElementById("app");
 ReactDOM.render(template, appRoot);
+
+var user = {
+  Name: "praveen",
+  Age: 18,
+  Location: "Delhi"
+
+};
+function getLoc(location) {
+  if (location) return React.createElement(
+    "p",
+    null,
+    "location: ",
+    location
+  );
+}
+var temp = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    user.Name ? user.Name : "NONAME"
+  ),
+  user.Age && user.Age >= 18 && React.createElement(
+    "p",
+    null,
+    "Age:",
+    user.Age
+  ),
+  getLoc(user.Location)
+);
+
+var tempTwo = document.getElementById("personal");
+ReactDOM.render(temp, tempTwo);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,27 +1,44 @@
 console.log("hello");
 ///////////JSX  Java Script XML
-  var studentInfo=
-(
-      <div>
-            <h1>Name: Arpit</h1>
-            <p> Age:20</p>
-            <p>Location:Bareilly</p>
-      </div>
-);
-      var templateTwo=document.getElementById("personal");
-      ReactDOM.render(studentInfo,templateTwo);
+var app={
+     title:"App",
+     subtitle:"this app is very useful",     
+     options:['one','two']
 
-
-      var template=
-<div>
-    <h1>INDECISION APP</h1>
-    <p>this is your jsx code</p>
+}
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle&&<p>{app.subtitle}</p>}
+    {app['options'].length>0 ?<p>Here are Your Options</p> :<p>No Options </p> }
     <ol>
-        <li>HTML</li>
-        <li>CSS</li>
+      <li>Item One</li>
+      <li>Item Two</li>
     </ol>
-</div>;
-var appRoot=document.getElementById("app");
-  ReactDOM.render(template,appRoot);
+  </div>
+);
+var appRoot = document.getElementById("app");
+ReactDOM.render(template, appRoot);
 
-  
+var user=
+{
+      Name:"praveen",
+      Age:18,
+      Location:"Delhi"
+
+}
+function getLoc(location)
+{
+      if(location)
+      return <p>location: {location}</p>;
+}
+var temp = (
+  <div>
+    <h1>{user.Name ?user.Name:"NONAME"}</h1>
+    {user.Age &&(user.Age >=18 ) &&<p>Age:{user.Age}</p>}
+    {getLoc(user.Location)}
+  </div>
+);
+
+var tempTwo = document.getElementById("personal");
+ReactDOM.render(temp, tempTwo);
