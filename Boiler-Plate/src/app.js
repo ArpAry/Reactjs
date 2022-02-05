5293b51c25544e0d099885fbae3640d364f30db5
ArpAry <arpitarya3299@gmail.com>
2022-02-05 13:58:22 +0530

    setting up a Boiler Plate and Expensify App

---
+++ Boiler-Plate/src/app.js
@@ -0,0 +1,7 @@
+// import validator from "validator";
+import React from "react";
+import ReactDOM from "react-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+const appRoot = document.getElementById("app");
+ReactDOM.render(<p>this is Boilerplate</p>, appRoot);
