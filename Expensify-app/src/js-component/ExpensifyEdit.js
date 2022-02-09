db478e7f33c2fd508b6da75c1b8a14697c7968bf
ArpAry <arpitarya3299@gmail.com>
2022-02-10 00:03:08 +0530

    using params HOOK first time

--- Expensify-app/src/js-component/ExpensifyEdit.js
+++ Expensify-app/src/js-component/ExpensifyEdit.js
@@ -1,10 +1,14 @@
 import React from "react";
 import {useParams } from 'react-router-dom';
-const ExpensifyEdit = (props) => {
+import { useLocation } from 'react-router-dom';
+const ExpensifyEdit = () => {
     let {id}=useParams();
+    let location = useLocation();
+    console.log(location)
     console.log(id);
     return(
-        <div>  Edit the Expense  Page with id of {id}
+        <div> 
+         Edit the Expense  Page with id of {id}
         </div>
     )
 };
