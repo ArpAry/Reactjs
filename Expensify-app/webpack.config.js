6cefad3da217fbfc0015aeead2faf91f5b932be5
ArpAry <arpitarya3299@gmail.com>
2022-02-16 22:41:33 +0530

    devloping Expensify-App

--- Expensify-app/webpack.config.js
+++ Expensify-app/webpack.config.js
@@ -1,47 +1,35 @@
 const path = require("path");
-module.exports = {"presets": "@babel/preset-env"}
+module.exports = { presets: "@babel/preset-env" };
 module.exports = {
-  
   mode: "development",
   entry: "./src/playground/redux-101.js",
 
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, "public"),
     filename: "bundle.js",
   },
 
-
   module: {
-         rules: [
-           {
-             loader: "babel-loader",
-             test: /\.js$/,
-             exclude: /node_modules/,
-           },
-           {
-             test:/\.s?css$/,
-             use:[
-               'style-loader',
-               'css-loader',
-               'sass-loader'
-              ],
-           }
-         ],
-       },
- 
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+
   devtool: "eval-cheap-module-source-map",
   devServer: {
     static: {
-         directory: path.join(__dirname, 'public')
+      directory: path.join(__dirname, "public"),
     },
     // In previous it is Require used search  path
 
-    
-   historyApiFallback:true
-    
-
-
-}
-
-  
+    historyApiFallback: true,
+  },
 };
