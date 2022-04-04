d4a39cfac109ce4643157dcae57cd526a987f05e
ArpAry <arpitarya3299@gmail.com>
2022-04-05 02:32:33 +0530

    reducer working with combine REducers

--- Expensify-app/src/playground/Redux-Expensify.js
+++ Expensify-app/src/playground/Redux-Expensify.js
@@ -1,6 +1,31 @@
-import {createStore,combineReducers } from 'redux';
+import {createStore, combineReducers } from 'redux';
+import {v4 as uuid} from 'uuid';
+//
+//ADDEXPENSE Action Generator
+//
+// const addExpense=({description="",note="",amount=0,createdAt=0}={})
+//   ( {type:'ADDEXPENSE',
+// expense:{
+//     id:uuid(),
+//     description,//Shorthand property
+//     note,//Shorthand property
+//     amount,//Shorthand property
+//     createdAt//Shorthand property
+//  }});
+const addExpense=({description="",note="",amount=100,createdAt=0}={}) =>
+({
+    type:'ADDEXPENSE',
+    expense:{
+        id:uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+});
+
 const demostate={
-   expenses: [{
+   expense: [{
        id:"kjdfksk",
        description:"January Rent",
        note:"This was last Rent for that site",
@@ -14,3 +39,39 @@ const demostate={
         Enddate:undefined
     }
 };
+
+// Expensify  Reducer
+const expensifyDefault=[];
+const expensifyReducer=(state =[],action) =>{
+switch(action.type)
+{   case 'ADDEXPENSE':
+    return state.concat(action.expense);
+    default:return state;}
+} 
+// Filter Reducer
+const defaultFilterReducer={
+    text:"",
+    sortby:"Date",
+    startdate:undefined,
+    enddate:undefined
+};
+const filterReducer=(state=defaultFilterReducer,action)=>{
+    switch(action.type)
+    {
+        default:
+            return state;
+    }
+}
+
+//Store Creation
+const store=createStore(
+    combineReducers({
+        expense:expensifyReducer,
+        filters:filterReducer
+    })
+    );
+
+    store.subscribe(()=>{console.log(store.getState())});
+
+    store.dispatch(addExpense({description:"RENT",amount:1000}));
+    
