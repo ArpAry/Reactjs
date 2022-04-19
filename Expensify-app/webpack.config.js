7d515390c5c8bc882969bc4f8e9c452140e54655
ArpAry <arpitarya3299@gmail.com>
2022-04-19 18:21:08 +0530

    Sort the element by objects like amount ,created in descending order

--- Expensify-app/webpack.config.js
+++ Expensify-app/webpack.config.js
@@ -1,40 +1,32 @@
 const path = require("path");
-module.exports = {"presets": "@babel/preset-env"}
+module.exports = { presets: "@babel/preset-env" };
 module.exports = {
-  
   mode: "development",
   entry: "./src/playground/Redux-Expensify.js",
 
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, "public"),
     filename: "bundle.js",
   },
 
-
   module: {
-         rules: [
-           {
-             loader: "babel-loader",
-             test: /\.js$/,
-             exclude: /node_modules/,
-           },
-           {
-             test:/\.s?css$/,
-             use:[
-               'style-loader',
-               'css-loader',
-               'sass-loader'
-              ],
-           }
-         ],
-       },
- 
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+
   devtool: "eval-cheap-module-source-map",
   devServer: {
     static: {
-         directory: path.join(__dirname, 'public')
-    }
-}
-
-  
+      directory: path.join(__dirname, "public"),
+    },
+  },
 };
