1c6d87d2f81cde752a3d7ff8cc7d0b718eefd370
ArpAry <arpitarya3299@gmail.com>
2022-12-22 13:05:18 +0530

    adding spotify files

--- Expensify-app/src/js-component/ExpenseListItem.js
+++ Expensify-app/src/js-component/ExpenseListItem.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from 'moment';
+import numeral from "numeral";
+//numeral.defaultFormat('₹0,0.00');
 const ExpenseListItem = ({ id,  description, amount, createdAt }) => (
   <div>
   <Link to={`\edit/${id}`}>
     <h1>{description}</h1>
     </Link> 
     <p>
-      {amount}-{createdAt}
+
+     {numeral(amount).format('$0,0.00')}--
+     {moment(createdAt).format("MMM Do YY") } 
+    
+    
     </p>
     
   </div>
